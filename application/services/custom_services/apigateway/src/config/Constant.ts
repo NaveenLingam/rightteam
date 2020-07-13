8ce756627c909cd419ff573e058c0d2906070f1f
naveenlingam <naveenlingam@10decoders.in>
2020-07-13 13:07:08 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://rightteam-app.rightteam.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://rightteam-app.rightteam.svc.cluster.local:3009';
+export const ADMINURL = 'http://rightteam-app.rightteam.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://rightteam-app.rightteam.svc.cluster.local:3008';
